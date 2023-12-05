f4e2b768fa7eecda344f97dbe22de91b23fd0457
Raphael Tostes <raphatm@live.com>
2023-12-05 18:26:36 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo Homem de Ferro em 3D',
+    description: 'marvel anuncia um novo filme do homem de ferro, confira.',
+    photoCover:
+      'https://t.ctcdn.com.br/wxhrauAIor8M57MPlegYe89XWjY=/640x360/smart/i798787.jpeg',
+  },
+  {
+    id: '2',
+    title: 'Nova série Anunciada no Disney +',
+    description: 'nova série vai ser maravilhosa, segundo diretor.',
+    photoCover:
+      'https://img.r7.com/images/loki1jpg-07072021185242704?dimensions=771x420',
+  },
+  {
+    id: '3',
+    title: 'As Marvels, uma surpresa pro universo MCU',
+    description: 'As Marvels promete neste filme espetacular!',
+    photoCover:
+      'https://www.otempo.com.br/image/contentid/policy:1.3269452:1699304729/image-Os-filmes-e-series-do-MCU-que-voce-precisa-ver-antes-de-As-Marvels.jpg',
+  },
+  {
+    id: '4',
+    title: 'Leia nossa resenha sobre Falcão e Soldado Invernal',
+    description: 'Ótima série com personagens não tão conhecidos assim!',
+    photoCover:
+      'https://files.meiobit.com/wp-content/uploads/2021/04/falcon-winter-soldier-001.jpg',
+  },
+];
